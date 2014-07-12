cad8edfc0ce65e4d1a56f2ebbf1af1a7b73f990d
zfq <1586687169@qq.com>
2014-07-12 23:26:27 +0800

    添加时间显示功能

--- 163评论/Content.m
+++ 163评论/Content.m
@@ -31,7 +31,64 @@
     _email = [dictionary objectForKey:@"u"];
     _content = [dictionary objectForKey:@"b"];
     _content = [NSString replaceBr:_content];
-    _time = [dictionary objectForKey:@"t"];
+//    _time = [dictionary objectForKey:@"t"];
+    _time = [self postTimeFromTime:[dictionary objectForKey:@"t"]];
+}
+
+- (NSString *)postTimeFromTime:(NSString *)time
+{
+    NSString *postTime = nil;
+    NSDate *currDate = [NSDate date];
+    
+    //-------------将time转换为NSDate----------
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"yyyy-MM-dd HH:mm:ss";
+    NSDate *date = [formatter dateFromString:time];
+    
+    NSTimeInterval interval = [currDate timeIntervalSinceDate:date];
+    
+    NSInteger oneDay = 24 * 60 * 60;
+    NSInteger oneHour = 60 * 60;
+    NSInteger oneMinute = 60;
+    if (interval < 2*oneDay)
+    {
+        if (interval < oneDay)
+        {
+            if (interval < oneHour)
+            {
+                if (interval < oneMinute)
+                    postTime = @"刚刚";
+                else
+                {
+                    postTime = [NSString stringWithFormat:@"%d分钟前",(NSInteger)(interval/oneMinute)];
+                }
+            }
+            else
+            {
+                formatter.dateFormat = @"HH:mm";
+                NSString *todayDateStr = [formatter stringFromDate:date];
+                postTime = [NSString stringWithFormat:@"今天 %@",todayDateStr];
+            }
+        }
+        else
+        {
+            postTime = @"昨天";
+        }
+    }
+    else
+    {
+        formatter.dateFormat = @"yyyy-MM-dd HH:mm";
+        NSString *dateStr = [formatter stringFromDate:date];
+        postTime = dateStr;
+    }
+
+    return postTime;
 }
 
 @end
+
+
+
+
+
+
