38be521383d29fb45c00efb269d57e8a1a36b330
zfq <1586687169@qq.com>
2014-11-18 12:29:58 +0800

    添加文件

---
+++ 163评论/RandomPostCell.h
@@ -0,0 +1,15 @@
+//
+//  RandomPostCell.h
+//  163评论
+//
+//  Created by zhaofuqiang on 14-11-18.
+//  Copyright (c) 2014年 zhaofuqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RandomPostCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *myContentLabel;
+@property (weak, nonatomic) IBOutlet UILabel *mySeparatorLabel;
+
+@end
---
+++ 163评论/RandomPostCell.m
@@ -0,0 +1,23 @@
+//
+//  RandomPostCell.m
+//  163评论
+//
+//  Created by zhaofuqiang on 14-11-18.
+//  Copyright (c) 2014年 zhaofuqiang. All rights reserved.
+//
+
+#import "RandomPostCell.h"
+
+@implementation RandomPostCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
